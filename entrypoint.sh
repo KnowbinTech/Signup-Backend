a8a2732b328bd22977021529cc6772fbf0f62eee
athulmgeorge <119030486+athulmgeorge@users.noreply.github.com>
2024-10-31 04:27:23 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Applying migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn e_commerce.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn e_commerce.wsgi:application --bind 0.0.0.0:${PORT:-8001} --workers 3
