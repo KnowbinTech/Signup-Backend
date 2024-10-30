adc4bb2136fac0f9a21797ed7bb1ef0abd77e394
athulmgeorge <athulmgeorge12@gmail.com>
2024-10-31 03:31:38 +0530

    added cors

--- e_commerce/settings/local.py
+++ e_commerce/settings/local.py
@@ -10,19 +10,25 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost:8080",
     "localhost:5173",
-    "127.0.0.1:5173"
+    "127.0.0.1:5173",
+    "https://signupcasuals.com",
+    "https://manage.signupcasuals.com"
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    "https://signupcasuals.com",
+    "https://manage.signupcasuals.com"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    "https://signupcasuals.com",
+    "https://manage.signupcasuals.com"
 ]
 
 SECURE_BROWSER_XSS_FILTER = True
