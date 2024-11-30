d9d4de1e4bd600d6cf55f4948317ea808c2d0e54
Rahul M U <150176083+rmu-work@users.noreply.github.com>
2024-12-01 04:57:34 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,8 @@ python manage.py migrate --noinput
 
 echo "Starting Gunicorn..."
 exec gunicorn e_commerce.wsgi:application \
-    --bind 0.0.0.0:${PORT:-8001} \
-    --workers 3 \
-    --threads 2 \
-    --timeout 120 \
+    --bind 0.0.0.0:8001 \
+    --workers 4 \
+    --threads 3 \
     --access-logfile - \
     --error-logfile -
