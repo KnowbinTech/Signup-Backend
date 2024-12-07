8968791b66184b1cf2eafda61a93d74d2f5e1d15
jishnu <jishnuks9421@gmail.com>
2024-12-07 15:36:53 +0530

    filter bug in user resolved

--- users/filters.py
+++ users/filters.py
@@ -1,6 +1,7 @@
 import django_filters as filters
 
 from users.models import AddressRegister
+from users.models import User
 
 
 class AddressRegisterFilter(filters.FilterSet):
@@ -8,4 +9,8 @@ class AddressRegisterFilter(filters.FilterSet):
         model = AddressRegister
         fields = ['user']
 
+class UserFilter(filters.FilterSet):
+    class Meta:
+        model = User
+        fields = ["name",'username']
 
